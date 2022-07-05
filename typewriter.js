c24b835d1eefff11c18f19d236d3a92074d7f027
Sszyh <zhuyer@outlook.com>
2022-07-05 15:00:09 -0700

    finalygit add typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -4,10 +4,16 @@ let time = 0;
 
 for (const a of sentence) {
   setTimeout(() => {
-    process.stdout.write(a + "\n"); 
+    process.stdout.write(a); 
+    //console.log(a);
   }, time);
   time += 50;
   
 }
-//process.stdout.write("\n")
+setTimeout(( )=> {
+  process.stdout.write("\n")
+} , time);
+//Tip: We can use console.log, or write \n to stdout. That said, we can't output this character until all the characters are written out to stdout.
 
+
+//(sentence.length * 50)
